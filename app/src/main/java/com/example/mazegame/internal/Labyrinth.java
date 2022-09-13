310c8513a3ed5b45d55983bae42533e4e46daac6
Joao Guimaraes <guimaraes.joao.v@gmail.com>
2022-09-13 20:14:13 -0300

    Added custom view DrawView.java to project activity_game.xml

---
+++ app/src/main/java/com/example/mazegame/internal/Labyrinth.java
@@ -0,0 +1,228 @@
+package com.example.mazegame.internal;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+public class Labyrinth {
+  private final Random random = new Random(Instant.now().getEpochSecond());
+  private int rows;
+  private int cols;
+  private int canvasWidth;
+  private int canvasHeight;
+  private int squareLength;
+  private Vector<Vector<Cell>> cells;
+  private Vector<Edge> edges;
+
+  public Vector<Vector<Cell>> getCells() {
+    return cells;
+  }
+
+  public void setCells(Vector<Vector<Cell>> cells) {
+    this.cells = cells;
+  }
+
+  public int getRows() {
+    return rows;
+  }
+
+  public void setRows(int rows) {
+    this.rows = rows;
+  }
+
+  public int getCols() {
+    return cols;
+  }
+
+  public void setCols(int cols) {
+    this.cols = cols;
+  }
+
+  public int getCanvasWidth() {
+    return canvasWidth;
+  }
+
+  public void setCanvasWidth(int canvasWidth) {
+    this.canvasWidth = canvasWidth;
+    squareLength = Math.min(canvasWidth, canvasHeight) / cols;
+  }
+
+  public int getCanvasHeight() {
+    return canvasHeight;
+  }
+
+  public void setCanvasHeight(int canvasHeight) {
+    this.canvasHeight = canvasHeight;
+    squareLength = Math.min(canvasWidth, canvasHeight) / rows;
+  }
+
+  public Vector<Edge> getEdges() {
+    return edges;
+  }
+
+  public int getSquareLength() {
+    return squareLength;
+  }
+
+  public Labyrinth(int rows, int cols, int canvasWidth, int canvasHeight) {
+    this.rows = rows;
+    this.cols = cols;
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    squareLength = Math.min(canvasWidth, canvasHeight) / Math.min(rows, cols);
+  }
+
+  public void buildLabyrinth() {
+    createCells(rows, cols);
+    createEdges(rows, cols);
+    createLabyrinthAldousBroder();
+  }
+
+  public void createEdges(int rows, int cols) {
+    this.edges = new Vector<>();
+    for (int i = 0; i < rows + 1; i++) {
+      for (int j = 0; j < cols; j++) {
+        Edge e = new Edge(i, j, i, (j + 1), getSquareLength());
+        if (i - 1 >= 0) {
+          e.getCells().add(cells.get(i - 1).get(j));
+        }
+        if (i < rows) {
+          e.getCells().add(cells.get(i).get(j));
+        }
+        edges.add(e);
+      }
+    }
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols + 1; j++) {
+        Edge e = new Edge(i, j, (i + 1), j, getSquareLength());
+        if (j - 1 >= 0) {
+          e.getCells().add(cells.get(i).get(j - 1));
+        }
+        if (j < cols) {
+          e.getCells().add(cells.get(i).get(j));
+        }
+        edges.add(e);
+      }
+    }
+  }
+
+  private void createCells(int rows, int cols) {
+    this.cells = new Vector<>(rows);
+
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < rows; i++, y += getSquareLength()) {
+      Vector<Cell> row = new Vector<>(cols);
+      for (int j = 0; j < cols; j++, x += getSquareLength()) {
+        row.add(new Cell(x, y, i, j, false));
+      }
+      this.cells.add(row);
+    }
+  }
+
+  private void createLabyrinthAldousBroder() {
+    int remaining = (rows * cols) - 1;
+    Cell current = cells.get(random.nextInt(rows)).get(random.nextInt(cols));
+    current.setVisited(true);
+    while (remaining > 0) {
+      Cell next = getRandomNeighbor(current);
+      if (!next.isVisited()) {
+        Edge wall = getWall(current, next);
+        wall.setVisible(false);
+        next.setVisited(true);
+        remaining--;
+      }
+      current = next;
+    }
+  }
+
+  public void resizeCells() {
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        Cell c = cells.get(i).get(j);
+        c.setX(j * getSquareLength());
+        c.setY(i * getSquareLength());
+      }
+    }
+  }
+
+  public void resizeEdges() {
+    for (Edge edge : edges) {
+      edge.setLength(getSquareLength());
+    }
+  }
+
+  private Edge getWall(Cell a, Cell b) {
+    for (Edge e : edges) {
+      if (e.getCells().contains(a) && e.getCells().contains(b)) {
+        return e;
+      }
+    }
+    throw new RuntimeException(String.format("Could not find wall between %s and %s", a.toString(), b.toString()));
+  }
+
+  private Cell getRandomNeighbor(Cell cell) {
+    Vector<Cell> neighbors = getCellNeighbors(cell);
+    Cell neighbor = neighbors.get(random.nextInt(neighbors.size()));
+    return cells.get(neighbor.getRow()).get(neighbor.getCol());
+  }
+
+  private Vector<Edge> getWalls(Cell cell) {
+    Edge e1 = new Edge(cell.getRow(), cell.getCol(), cell.getRow(), cell.getCol() + 1, getSquareLength());
+    Edge e2 = new Edge(cell.getRow(), cell.getCol(), cell.getRow() + 1, cell.getCol(), getSquareLength());
+    Edge e3 = new Edge(cell.getRow(), cell.getCol() + 1, cell.getRow() + 1, cell.getCol() + 1, getSquareLength());
+    Edge e4 = new Edge(cell.getRow() + 1, cell.getCol(), cell.getRow() + 1, cell.getCol() + 1, getSquareLength());
+    List<Edge> list = new ArrayList<>();
+    list.add(e1);
+    list.add(e2);
+    list.add(e3);
+    list.add(e4);
+    return edges.stream()
+      .filter((edge) -> list.contains(edge) && edge.isVisible()).collect(Collectors.toCollection(Vector::new));
+  }
+
+  private Vector<Cell> getCellNeighbors(Cell cell) {
+    int row = cell.getRow();
+    int col = cell.getCol();
+    Vector<Cell> walls = new Vector<>();
+    if (row > 0 && row < rows - 1 && col > 0 && col < cols - 1) {
+      walls.add(cells.get(row - 1).get(col));
+      walls.add(cells.get(row).get(col + 1));
+      walls.add(cells.get(row).get(col - 1));
+      walls.add(cells.get(row + 1).get(col));
+    } else if (row == 0 && col > 0 && col < cols - 1) {
+      walls.add(cells.get(row).get(col + 1));
+      walls.add(cells.get(row).get(col - 1));
+      walls.add(cells.get(row + 1).get(col));
+    } else if (row == rows - 1 && col > 0 && col < cols - 1) {
+      walls.add(cells.get(row - 1).get(col));
+      walls.add(cells.get(row).get(col + 1));
+      walls.add(cells.get(row).get(col - 1));
+    } else if (row > 0 && row < rows - 1 && col == 0) {
+      walls.add(cells.get(row - 1).get(col));
+      walls.add(cells.get(row).get(col + 1));
+      walls.add(cells.get(row + 1).get(col));
+    } else if (row > 0 && row < rows - 1 && col == cols - 1) {
+      walls.add(cells.get(row - 1).get(col));
+      walls.add(cells.get(row).get(col - 1));
+      walls.add(cells.get(row + 1).get(col));
+    } else if (row == 0 && col == 0) {
+      walls.add(cells.get(row).get(col + 1));
+      walls.add(cells.get(row + 1).get(col));
+    } else if (row == rows - 1 && col == 0) {
+      walls.add(cells.get(row - 1).get(col));
+      walls.add(cells.get(row).get(col + 1));
+    } else if (row == 0 && col == cols - 1) {
+      walls.add(cells.get(row).get(col - 1));
+      walls.add(cells.get(row + 1).get(col));
+    } else if (row == rows - 1 && col == cols - 1) {
+      walls.add(cells.get(row - 1).get(col));
+      walls.add(cells.get(row).get(col - 1));
+    }
+
+    return walls;
+  }
+}
---
+++ app/src/main/java/com/example/mazegame/view/DrawView.java
@@ -0,0 +1,89 @@
+package com.example.mazegame.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.example.mazegame.internal.Cell;
+import com.example.mazegame.internal.Edge;
+import com.example.mazegame.internal.Labyrinth;
+import com.example.mazegame.internal.Player;
+import com.example.mazegame.R;
+
+public class DrawView extends View {
+  private final Paint paint = new Paint();
+  private final Integer cellColor;
+  private final Integer edgeColor;
+  private final Integer nonCellColor;
+  private final Integer playerColor;
+  private Labyrinth labyrinth;
+  private Integer labyrinthRows;
+  private Integer labyrinthCols;
+  private Player player;
+
+  public DrawView(Context context, AttributeSet attr) {
+    super(context, attr);
+    TypedArray typedArray = context.getTheme().obtainStyledAttributes(attr, R.styleable.DrawView, 0, 0);
+    cellColor = typedArray.getColor(R.styleable.DrawView_cellColor, Color.BLACK);
+    edgeColor = typedArray.getColor(R.styleable.DrawView_edgeColor, Color.WHITE);
+    nonCellColor = typedArray.getColor(R.styleable.DrawView_nonCellColor, Color.RED);
+    playerColor = typedArray.getColor(R.styleable.DrawView_playerColor, Color.RED);
+    labyrinthRows = typedArray.getInteger(R.styleable.DrawView_labyrinthRows, 20);
+    labyrinthCols = typedArray.getInteger(R.styleable.DrawView_labyrinthCols, 20);
+    labyrinth = new Labyrinth(labyrinthRows, labyrinthCols, 200, 200);
+    labyrinth.buildLabyrinth();
+    player = new Player(15, 15, labyrinth.getSquareLength()/5);
+  }
+
+  @Override
+  public void onDraw(Canvas canvas) {
+    paint.setStyle(Paint.Style.FILL);
+    for (int i = 0; i < labyrinth.getRows(); i++) {
+      for (int j = 0; j < labyrinth.getCols(); j++) {
+        Cell c = labyrinth.getCells().get(i).get(j);
+        paint.setColor(c.isMaze() ? nonCellColor : cellColor);
+        canvas.drawRect(c.getX(), c.getY(), c.getX() + labyrinth.getSquareLength(), c.getY() + labyrinth.getSquareLength(), paint);
+      }
+    }
+
+    paint.setStyle(Paint.Style.STROKE);
+    paint.setColor(edgeColor);
+    for (Edge e : labyrinth.getEdges()) {
+      if (e.isVisible()) {
+        float x0 = e.getX0() * labyrinth.getSquareLength();
+        float y0 = e.getY0() * labyrinth.getSquareLength();
+        float x = e.getX() * labyrinth.getSquareLength();
+        float y = e.getY() * labyrinth.getSquareLength();
+        canvas.drawLine(x0 - 2, y0 - 2, x - 2, y - 2, paint);
+        canvas.drawLine(x0 - 1, y0 - 1, x - 1, y - 1, paint);
+        canvas.drawLine(x0, y0, x, y, paint);
+        canvas.drawLine(x0 + 1, y0 + 1, x + 1, y + 1, paint);
+        canvas.drawLine(x0 + 2, y0 + 2, x + 2, y + 2, paint);
+      }
+    }
+
+    paint.setStyle(Paint.Style.FILL);
+    paint.setColor(playerColor);
+    canvas.drawCircle(player.getX(), player.getY(), player.getRadius(), paint);
+  }
+
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+    labyrinth.setCanvasWidth(w);
+    labyrinth.setCanvasHeight(h);
+    labyrinth.resizeCells();
+    labyrinth.resizeEdges();
+    player.setRadius(labyrinth.getSquareLength()/5);
+    invalidate();
+  }
+}
